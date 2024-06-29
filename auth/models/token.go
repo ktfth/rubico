3eeaad869a024a86123ff98e13c82d078a6d9a73
Kaique da Silva <kaeyosthaeron@gmail.com>
2024-06-28 23:55:50 -0300

    Added necessary deps for database

--- auth/models/token.go
+++ auth/models/token.go
@@ -1,14 +1,13 @@
 package models
 
 import (
-	"time"
-
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 type Token struct {
-	ID        primitive.ObjectID `bson:"_id"`
+	ID        primitive.ObjectID `bson:"_id" json:"id,omitempty"`
 	Token     string             `bson:"token"`
 	ExpiresAt time.Time          `bson:"expiresAt"`
-	UserID    primitive.ObjectID `bson:"userId"`
+	UserID    primitive.ObjectID `bson:"userId" json:"userId,omitempty"`
 }
